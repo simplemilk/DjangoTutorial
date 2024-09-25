db4da60c1e2648850f0623ff5e7e9a1e6133cce6
simplemilk <jjpark1@my.waketech.edu>
2024-09-24 21:49:16 -0400

    Itegrating Custom User Model
    Django Tutorial FInal section omgggggggg

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
-from .models import Room
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, User
 
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+ 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
@@ -11,5 +17,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = '__all__'
-        exclude = ['username', 'email']
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
